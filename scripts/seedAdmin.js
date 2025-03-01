8d1ac83905f480905a4a68d3f9aa8bdc007b7988
Devraj-098765 <119732347+Devraj-098765@users.noreply.github.com>
2025-03-01 11:48:05 +0545

    Add script to seed admin user with hashed password

---
+++ scripts/seedAdmin.js
@@ -0,0 +1,26 @@
+import bcrypt from "bcrypt";
+import { Admin } from "../models/admin.js";
+
+const seedAdmin = async () => {
+  const adminData = {
+    email: "admin@example.com",
+    password: "admin123",
+    role: "admin",
+  };
+
+  // Check if the admin already exists
+  let admin = await Admin.findOne({ email: adminData.email });
+  if (!admin) {
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    adminData.password = await bcrypt.hash(adminData.password, salt);
+
+    // Create and save the admin user
+    admin = new Admin(adminData);
+    await admin.save();
+  } else {
+    console.log("Fixed admin user already exists.");
+  }
+};
+
+export default seedAdmin;
