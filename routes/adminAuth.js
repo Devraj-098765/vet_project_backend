1e20ac3aa8999d467eba4482e5301758cf47e834
Devraj-098765 <119732347+Devraj-098765@users.noreply.github.com>
2025-03-01 11:46:34 +0545

    Add admin authentication route with validation and token generation

---
+++ routes/adminAuth.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { Admin } from "../models/admin.js";
+import _ from "lodash";
+import bcrypt from "bcrypt";
+import { z } from "zod";
+
+const adminAuthRouter = express.Router();
+
+adminAuthRouter.post("/", async (req, res) => { 
+    const { error } = validate(req.body);
+
+    if (error) return res.status(400).json({ errors: error.errors });
+
+    const { email, password } = req.body;
+
+    // check if admin exists
+    let admin = await Admin.findOne({ email });
+    if (!admin) return res.status(400).json({ message: "Invalid email or password" });
+
+    // validats the password
+    const validPassword = await bcrypt.compare(password, admin.password);
+    if (!validPassword) return res.status(400).json({ message: "Invalid email or password" });
+
+    // generate token && send it at the header
+    const token = admin.generateAuthToken();
+    res.header("x-auth-token", token).send(token);
+});
+
+const validate = (admin) => {
+  const schema = z.object({
+    email: z.string().email({ message: "Invalid email format"}).min(5, { message: "email is required" }).max(255),
+    password: z.string().min(5, { message: "Password is required" }).max(255),
+  });
+
+  return schema.safeParse(admin);
+};
+
+export default adminAuthRouter;
