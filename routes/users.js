0165580c8017d60bbadf9034dae40bb4126dbdae
Devraj-098765 <119732347+Devraj-098765@users.noreply.github.com>
2025-03-13 11:17:55 +0545

    Refactor user route for improved readability and consistency; remove unnecessary newlines and comments

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,3 @@
-
 import express from "express";
 import { User, validateUser } from "../models/user.js";
 import _ from "lodash";
@@ -12,18 +11,16 @@ userRouter.get("/", async (req, res) => {
     const users = await User.find().select("name email");
     console.log(users);
     res.send(users);
-  }
-  catch (error) {
+  } catch (error) {
     res.status(500).send("Internal Server Error");
   }  
 });
 
 userRouter.post("/", async (req, res) => {
   const { error } = validateUser(req.body);
-
   if (error) return res.status(400).json({ errors: error.errors });
 
-  let user = await User.findOne({ email: req.body.email }); // returns a promise we await it
+  let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registered");
 
   user = new User(_.pick(req.body, ["name", "email", "password"]));
@@ -32,15 +29,13 @@ userRouter.post("/", async (req, res) => {
   await user.save();
 
   const token = user.generateAuthToken();
-  console.log("Generated Token:", token); // Debugging
+  console.log("Generated Token:", token);
 
-  // Check for newline characters
   if (token.includes("\n")) {
     console.error("Token contains newline characters!");
   }
 
   res.header('x-auth-token', token).send(_.pick(user, ["_id", "name", "email"]));
 });
-
 
 export default userRouter;
