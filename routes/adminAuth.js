7ab1c5bf6d258502ec39c80858ecf336cae4afeb
Devraj-098765 <119732347+Devraj-098765@users.noreply.github.com>
2025-03-25 09:13:49 +0545

    Add role-based authentication for admin and veterinarian users

--- routes/adminAuth.js
+++ routes/adminAuth.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { Admin } from "../models/admin.js";
+import { Veterinarian } from "../models/Veterinarian.js";
 import _ from "lodash";
 import bcrypt from "bcrypt";
 import { z } from "zod";
-import c from "config";
 
 const adminAuthRouter = express.Router();
 
@@ -12,32 +12,37 @@ adminAuthRouter.post("/", async (req, res) => {
 
     if (error) return res.status(400).json({ errors: error.errors });
 
-    const { email, password } = req.body;
+    const { email, password, role } = req.body;
 
-    // check if admin exists
-    let admin = await Admin.findOne({ email });
-    if (!admin) return res.status(400).json({ message: "Invalid email or password" });
+    let user;
+    if (role === "admin") {
+      user = await Admin.findOne({ email });
+    } else if (role === "vetnerian") {
+      user = await Veterinarian.findOne({ email: req.body.email }); 
+    } else {
+      return res.status(400).json({ message: "Invalid role" });
+    }
+    
+    console.log(user);
+    if (!user) return res.status(400).json({ message: "Invalid email or password" });
+
 
     // validats the password
-    const validPassword = await bcrypt.compare(password, admin.password);
+    const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) return res.status(400).json({ message: "Invalid email or password" });
 
     // generate token && send it at the header
-    const token = admin.generateAuthToken();
-    console.log("Generated Token:", token); // Debugging
-    res.header("x-auth-token", token).send(token);
+    const token = user.generateAuthToken();
+    res.header('x-auth-token', token).send(_.pick(user, ["_id", "email", "role"]));
 });
 
-const validate = (admin) => {
+const validate = (data) => {
   const schema = z.object({
     email: z.string().email({ message: "Invalid email format"}).min(5, { message: "email is required" }).max(255),
     password: z.string().min(5, { message: "Password is required" }).max(255),
   });
 
-  return schema.safeParse(admin);
+  return schema.safeParse(data);
 };
 
 export default adminAuthRouter;
-
-
-
